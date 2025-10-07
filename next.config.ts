ece58378ddf32baedc778d43a6827101c075c963
ruckyTheWebDesigner <rukewejoseph77@gmail.com>
2025-10-07 16:51:11 +0200

    Update remote image patterns in next.config.ts to include additional source

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://images.pexels.com/**")],
+    remotePatterns: [new URL("https://images.pexels.com/**"), new URL("https://o7y30yptvf.ufs.sh/**")],
   },
 };
 
